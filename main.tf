0348db7f1fb397a018f30ab29384f6b10160ed6d
CDanielTavares <99422412+CDanielTavares@users.noreply.github.com>
2025-03-07 21:07:49 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+	region = "eu-west-1"
+}
+
+resource "aws_instance" "example" {
+	ami 			= "ami-0a89fa9a6d8c7ad98"
+	instance_type 	= "t2.micro"
+	
+	tags = {
+		Name = "terraform-example"
+	}
+}
